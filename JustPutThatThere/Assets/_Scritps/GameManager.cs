291c38246d78dd2d9c6d7605abc4a15605d77c73
LeducBastien <leduc.bastien.info@gmail.com>
2018-04-10 12:03:16 +0200

    Added game manager and pustule button

---
+++ JustPutThatThere/Assets/_Scritps/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager Instance;
+    public bool pustuleButtonActivated = false;
+    public bool gameOver = false;
+
+    private void Awake()
+    {
+        if (Instance) return;
+        Instance = this;
+    }
+
+}
---
+++ JustPutThatThere/Assets/_Scritps/PustuleButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PustuleButton : MonoBehaviour {
+
+    [SerializeField] List<Transform> spawnPoint;
+    [SerializeField] GameObject pustuleButtonPrefab;
+
+    private void Start()
+    {
+        SpawnPustuleButton();
+    }
+
+    public void SpawnPustuleButton()
+    {
+        GameObject pustuleButtonInstance = Instantiate(pustuleButtonPrefab);
+        int randomSpawn = Random.Range(0, spawnPoint.Count);
+        pustuleButtonInstance.transform.position = spawnPoint[randomSpawn].position;
+        GameManager.Instance.pustuleButtonActivated = true;
+    }
+
+}
