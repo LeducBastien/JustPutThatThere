49e4aba762579e9b63963046837193f351d8b1bc
LeducBastien <leduc.bastien.info@gmail.com>
2018-04-10 12:25:26 +0200

    Help button

--- JustPutThatThere/Assets/_Scritps/GameManager.cs
+++ JustPutThatThere/Assets/_Scritps/GameManager.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     public static GameManager Instance;
+
+    [SerializeField] GameObject helpButtonInstance;
+    [SerializeField] float timeBeforeHelp;
+    private float timer = 0;
     public bool pustuleButtonActivated = false;
     public bool gameOver = false;
+
+
 
     private void Awake()
     {
         if (Instance) return;
         Instance = this;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeBeforeHelp)
+            helpButtonInstance.SetActive(true);
     }
 
 }
