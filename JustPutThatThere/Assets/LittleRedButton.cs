2165b869499ff6cababbb0d45aa4f58acb341491
MDZ-PC\max <m.droulez@student.isartdigital.com>
2018-04-10 11:07:23 +0200

    added littleredbutton bigbutton reset numpad

---
+++ JustPutThatThere/Assets/LittleRedButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LittleRedButton : MonoBehaviour {
+
+    [SerializeField] GameObject bigRedButton;
+
+    public void SpawnBigRedButton ()
+    {
+        bigRedButton.SetActive(true);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ JustPutThatThere/Assets/NumPad.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumPad : MonoBehaviour {
+
+    private string value;
+
+    private readonly List<string> codes = new List<string>();
+
+
+    [SerializeField] Text display;
+
+    NumPad()
+    {
+        codes.Add("34");
+        codes.Add("996678");
+    }
+
+    public void Reset()
+    {
+        value = "";
+        display.text = value;
+    }
+
+    public void ButtonPressed (string digit)
+    {
+        value += digit;
+        display.text = value;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
