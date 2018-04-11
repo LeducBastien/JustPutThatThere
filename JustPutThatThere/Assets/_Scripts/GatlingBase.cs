257f2aff9c323809acafd68de37079c687bf0d51
MDZ-PC\max <m.droulez@student.isartdigital.com>
2018-04-11 12:25:19 +0200

    prefabs arm canonentrance

---
+++ JustPutThatThere/Assets/_Scripts/GatlingBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatlingBase : MonoBehaviour {
+
+    private const float DOWN_SPEED = 25f;
+    private const float DOWN_DISTANCE = 80f;
+
+    private float startingY;
+
+    private Action doAction;
+	// Use this for initialization
+	void Start () {
+        doAction = DoActionVoid;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        doAction();
+	}
+
+    public void StartEntrance()
+    {
+        startingY = transform.position.y;
+        doAction = DoActionEntrance;
+    }
+
+    private void DoActionEntrance()
+    {
+        var position = transform.position;
+        if(position.y < startingY - DOWN_DISTANCE)
+        {
+            position.y = startingY - DOWN_DISTANCE;
+            EntranceFinished();
+        }
+        else
+        {
+            position.y -= DOWN_SPEED * Time.deltaTime;
+        }
+        transform.position = position;
+    }
+
+    private void EntranceFinished()
+    {
+        doAction = DoActionVoid;
+    }
+
+    private void DoActionVoid()
+    {
+
+    }
+}
