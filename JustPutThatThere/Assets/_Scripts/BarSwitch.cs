092320fc7a09bac9a41533ebc6fe85f74a8a28ab
MDZ-PC\max <m.droulez@student.isartdigital.com>
2018-04-11 10:56:14 +0200

    added arm armdescender barfall

---
+++ JustPutThatThere/Assets/_Scripts/BarSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarSwitch : MonoBehaviour {
+
+    [SerializeField] Sprite onSprite;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Clicked()
+    {
+        GameManager.Instance.barFell = true;
+        GetComponent<SpriteRenderer>().sprite = onSprite;
+    }
+}
