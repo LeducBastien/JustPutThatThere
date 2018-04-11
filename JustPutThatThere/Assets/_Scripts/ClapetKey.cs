f4a79ff20305d1851196996df9bf86b87bb9d792
MDZ-PC\max <m.droulez@student.isartdigital.com>
2018-04-11 17:38:51 +0200

    prepared clapet key

--- JustPutThatThere/Assets/_Scripts/ClapetKey.cs
+++ JustPutThatThere/Assets/_Scripts/ClapetKey.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClapetKey : MonoBehaviour {
 
     [SerializeField] GatlingDoor gatlingDoor;
     [SerializeField] GameObject gatlingController;
+    private Image image;
+    [SerializeField] Sprite onSprite;
 	// Use this for initialization
 	void Start () {
-		
+        image = GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
@@ -25,5 +28,7 @@ public class ClapetKey : MonoBehaviour {
         }
 
         gatlingController.SetActive(true);
+        image.sprite = onSprite;
+
     }
 }
